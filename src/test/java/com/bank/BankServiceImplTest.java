218c252e0ca60ab51ddb2e38b8082e547baaf869
Chris <christoffer.radenholt@hotmail.se>
2023-02-15 13:11:55 +0100

    clean-up unnecessary lines of code

--- src/test/java/com/bank/BankServiceImplTest.java
+++ src/test/java/com/bank/BankServiceImplTest.java
@@ -4,13 +4,8 @@ import com.bank.service.impl.BankServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 public class BankServiceImplTest {
 
     private BankServiceImpl bankServiceImpl;
