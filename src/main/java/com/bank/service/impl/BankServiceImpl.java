7a060d2cdd6d02c3853b87b711615b10e163522a
Chris <christoffer.radenholt@hotmail.se>
2023-02-15 13:09:13 +0100

    adjusting deposit and withdraw to not include negative numbers

--- src/main/java/com/bank/service/impl/BankServiceImpl.java
+++ src/main/java/com/bank/service/impl/BankServiceImpl.java
@@ -6,7 +6,7 @@ import com.bank.service.BankService;
 public class BankServiceImpl implements BankService {
     @Override
     public void withdraw(BankAccount bankAccount, double amount) {
-        if (amount != 0 && bankAccount.getBalance() >= amount) {
+        if (amount > 0 && bankAccount.getBalance() >= amount) {
             bankAccount.setBalance(bankAccount.getBalance() - amount);
             bankAccount.setPrevTrans(bankAccount.getPrevTrans() - amount);
         } else if (bankAccount.getBalance() < amount) {
@@ -16,7 +16,7 @@ public class BankServiceImpl implements BankService {
 
     @Override
     public void deposit(BankAccount bankAccount, double amount) {
-        if (amount != 0) {
+        if (amount > 0) {
             bankAccount.setBalance(bankAccount.getBalance() + amount);
             bankAccount.setPrevTrans(amount);
         }
