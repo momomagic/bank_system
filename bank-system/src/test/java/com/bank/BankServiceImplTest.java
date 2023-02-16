1620e32426c5d719e4403e26d376945ebb691518
dzJP <113700179+dzJP@users.noreply.github.com>
2023-02-16 13:22:33 +0100

    add testWithdrawMethodBestCaseScenario()

--- bank-system/src/test/java/com/bank/BankServiceImplTest.java
+++ bank-system/src/test/java/com/bank/BankServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.bank;
 import com.bank.service.impl.BankServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,7 +17,21 @@ public class BankServiceImplTest {
     @Test
     public void setupBankAccount() {
         bankServiceImp = new BankServiceImpl();
-        bankAccount = new BankAccount("Pelle","1");
+        bankAccount = new BankAccount("Pelle", "1");
         bankAccount.balance = 50000.0;
+        System.out.println("Balance: " + bankAccount.getBalance());
+        Assertions.assertTrue(true,"Should turn true if account has any balance");
+    }
+
+    @Test
+    public void testWithdrawMethodBestCaseScenario() {
+        bankServiceImp = new BankServiceImpl();
+        bankAccount = new BankAccount("Pelle", "1");
+        bankAccount.balance = 50000.0;
+        bankServiceImp.withdraw(bankAccount, 25000.0);
+        System.out.println("Balance: " + bankAccount.getBalance());
+        Assertions.assertEquals(25000.0, 25000.0);
+
+
     }
 }
