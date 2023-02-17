c9c3b520e9b1806202cef5f4530abf46a24083bf
dzJP <113700179+dzJP@users.noreply.github.com>
2023-02-17 10:30:12 +0100

    added testBankServiceWithdrawShouldThrowRuneTimeException()

--- bank-system/src/test/java/com/bank/BankMenuTest.java
+++ bank-system/src/test/java/com/bank/BankMenuTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.Mockito.*;
@@ -15,6 +16,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class BankMenuTest {
     BankService bankServiceMock = mock(BankService.class);
+    BankAccount bankAccountMock = mock(BankAccount.class);
     BankAccount bankAccount;
 
 
@@ -26,7 +28,6 @@ public class BankMenuTest {
         bankServiceMock.withdraw(bankAccount,1000);
         verify(bankServiceMock).withdraw(any(BankAccount.class),anyDouble());
         Assertions.assertTrue(true,"Should return true if withdraw method was called");
-
     }
 
     @Test
@@ -35,5 +36,17 @@ public class BankMenuTest {
         bankServiceMock.deposit(bankAccount,1000);
         verify(bankServiceMock).deposit(any(BankAccount.class),anyDouble());
         Assertions.assertTrue(true,"Should return true if deposit method was called");
+    }
+
+    @Test
+    public void testBankServiceWithdrawShouldThrowRunTimeException() {
+        bankAccount = new BankAccount("1","pelle");
+        bankAccount.setBalance(1000);
+        doThrow(new RuntimeException()).when(bankServiceMock).withdraw(any(),anyDouble());
+        bankServiceMock.withdraw(bankAccount,1000);
+        assertThrows(RuntimeException.class, () -> {
+            bankServiceMock.withdraw(any(),anyDouble());
+        });
+
     }
 }
