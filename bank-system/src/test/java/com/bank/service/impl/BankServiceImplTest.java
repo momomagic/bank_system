466a8e108c6c6bd148f1252cdc9a898e030fa32c
aeneidITHS <112550107+aeneidITHS@users.noreply.github.com>
2023-02-21 19:38:55 +0100

    Adding BankServiceImplTest

---
+++ bank-system/src/test/java/com/bank/service/impl/BankServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.bank.service.impl;
+
+import junit.framework.TestCase;
+
+public class BankServiceImplTest extends TestCase {
+
+    public void testWithdraw() {
+
+    }
+
+    public void testDeposit() {
+    }
+}
